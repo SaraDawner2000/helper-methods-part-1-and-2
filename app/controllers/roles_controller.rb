26f7ea919c05533e2e1060c5b3abbbc229ddd5a2
Natalie Demyanenko <83845798+SaraDawner2000@users.noreply.github.com>
2024-06-25 13:24:36 -0500

    write create action in Roles controller

--- app/controllers/roles_controller.rb
+++ app/controllers/roles_controller.rb
@@ -8,12 +8,23 @@ class RolesController < ApplicationController
   end
 
   def new
+    @role = Role.new
   end
 
   def edit
+    @role = Role.find(params[:id])
   end
 
   def create
+    role_params = params.require(:role).permit(:actor_id, :movie_id, :character)
+    @role = Role.new(role_params)
+
+    if @role.valid?
+      @role.save
+      redirect_to role_url(@role), notice: "Role created successfully"
+    else
+      redirect_to role_url(@role), alert: "Role failed to create successfully"
+    end
   end
 
   def update
