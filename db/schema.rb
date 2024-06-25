9ca4972bb1416ba9573d9352f2f8328f6b11b961
Natalie Demyanenko <83845798+SaraDawner2000@users.noreply.github.com>
2024-06-25 11:02:03 -0500

    generate actor model

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2021_04_09_010404) do
+ActiveRecord::Schema[7.0].define(version: 2024_06_25_155246) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "actors", force: :cascade do |t|
+    t.string "name"
+    t.date "dob"
+    t.text "bio", default: "not available"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "movies", force: :cascade do |t|
     t.string "title"
